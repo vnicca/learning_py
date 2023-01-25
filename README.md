90e7ba3b34c8e2da22caf662f43b5945df8da00c
v_nica <pampushkaveronica@gmail.com>
2023-01-25 07:19:41 +0000

    Rock_Scissors_Paper_Lizard_Spock

--- README.md
+++
@@ -1,2 +0,0 @@
-# learning_py
-all_from_my_py_lessons
--- main.py
+++ main.py
@@ -1,71 +1,63 @@
+'''Rock_Scissors_Paper_Lizard_Spock'''
+import random
+flag = True
+while flag:
+    flag = False
 
-def gcd(m,n):
-    while m%n != 0:
-        oldm = m
-        oldn = n
-
-        m = oldn
-        n = oldm%oldn
-    return n
-
-class Fraction:
-    def __init__(self, top, bottom):
-        self.num = top
-        self.den = bottom
-
-    def show(self):
-        print(self.num, "/", self.den)
-
-    def __cmp__(self, arg):
-        if self > arg:
-            return -1
-        elif self == arg:
-            return 0
-        else:
-            return 1
-
-    def __str__(self):
-        return str(self.num) + "/" + str(self.den)
-
-    def Add(self, other):
-      new_num_a = self.num * other.den + \
-                self.den * other.num
-      new_den_a = self.den * other.den
-      common_a = gcd(new_num_a, new_den_a)
-      return Fraction(new_num_a//common_a, new_den_a//common_a)
-
-    def Sub(self, other):
-      new_num_s = self.num * other.den - \
-                self.den * other.num
-      new_den_s = self.den * other.den
-      common_s = gcd(new_num_s, new_den_s)
-      return Fraction(new_num_s//common_s, new_den_s//common_s)
+    player_choice = ''
+    bot_choice = ''
+    player_score = 0
+    bot_score = 0
     
-    def Mul(self, other):
-      new_num_m = self.num * other.num 
-      new_den_m = self.den * other.den
-      common_m = gcd(new_num_m, new_den_m)
-      return Fraction(new_num_m//common_m, new_den_m//common_m)
-     
+    for i in range(1, 5+1):
+        print(f'ROUND {i}')
+        player_choice = str(input('Enter choice : [r], [p], [s], [l], [k] :'))
+        bot_choice = random.choice('rpslk')
+        print(f'Player"s choice : {player_choice}.\nBot"s choice : {bot_choice} ')
 
-    def Div(self, other):
-      new_num_d = other.den * self.num 
-      new_den_d = self.den * other.num
-      common_d = gcd(new_num_d, new_den_d)
-      return Fraction(new_num_d//common_d, new_den_d//common_d)
-
-myf_1 = Fraction(4, 5)
-myf_2 = Fraction(3, 7)
-f3 = Fraction.Add(myf_1, myf_2)
-f4 = Fraction.Sub(myf_1, myf_2)
-f5 = Fraction.Mul(myf_1, myf_2)
-f6 = Fraction.Div(myf_1, myf_2)
-
-print('First fraction: ')
-myf_1.show()
-print('Second fraction: ')
-myf_2.show()
-print(myf_1, '+', myf_2, '=', f3)
-print(myf_1, '-', myf_2, '=', f4)
-print(myf_1, '*', myf_2, '=', f5)
-print(myf_1, ':', myf_2, '=', f6)
+        if player_choice == bot_choice:
+            print('Draw round')
+        elif player_choice == 'r':
+            if bot_choice == 'k' or bot_choice == 'p':
+                print('Bot wins the round')
+                bot_score = bot_score + 1
+            else:
+                print('Player wins the round')
+                player_score = player_score+1
+        elif player_choice == 'p':
+            if bot_choice == 'r' or bot_choice == 'k':
+                print('Player wins the round')
+                player_score = player_score + 1
+            else:
+                print('Bot wins the round')
+                bot_score = bot_score+1
+        elif player_choice == 's':
+            if bot_choice == 'p' or bot_choice == 'l':
+                print('Player wins the round')
+                player_score = player_score + 1
+            else:
+                print('Bot wins the round')
+                bot_score = bot_score+1
+        elif player_choice == 'k':
+            if bot_choice == 'r' or bot_choice == 's':
+                print('Player wins the round')
+                player_score = player_score + 1
+            else:
+                print('Bot wins the round')
+                bot_score = bot_score+1
+        elif player_choice == 'l':
+            if bot_choice == 'p' or bot_choice == 's':
+                print('Player wins the round')
+                player_score = player_score + 1
+            else:
+                print('Bot wins the round')
+                bot_score = bot_score+1
+    if player_score >  bot_score:
+        print(f'Player wins the battle with score : {player_score} :  {bot_score} ')
+    elif player_score <  bot_score:
+        print(f'Bot wins the battle with score : {bot_score} :  {player_score} ')
+    elif player_score ==  bot_score:
+        print(f'the score is equal : {player_score} :  {bot_score} ')
+    else:
+        print('Smthing is going wrong')
+    flag = bool(input("Press [Any key] + [Enter] to continue : "))
